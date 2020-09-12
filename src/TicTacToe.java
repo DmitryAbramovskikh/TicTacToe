98b91a79cfbc07c406201388c6809e02aa1e57f9
Dmitry Abramovskikh <dmabram15@gmail.com>
2020-09-12 22:28:17 +0300

    Rewrote checkWin and done AI

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -64,7 +66,55 @@ public class TicTacToe {
     }
 
     // aiTurn
+
+    /**
+     * Ход ИИ. ИИ делает предположение, если за 2 хода выигрыша или проигрыша нет - ходит случайно,
+     * иначе побеждает или блокирует победный ход человека
+     */
     private static void aiTurn() {
+        //Вспомогательное игровое поле на котором ИИ делает предположения
+        char[][] helpField = new char[field.length][field[0].length];
+        //Приведение вспомогательного игрового поля в состояние актуального для текущего состояния игры
+        for (int i = 0; i < field.length; i++) {
+            helpField[i] = field[i].clone();
+        }
+        /* Циклически для каждой пустой ячейки делаем предположения хода ИИ, если победа, то делаем победный ход,
+        если победного хода нет, то циклически делаем предположение о следующем ходе человека, если следующих ход
+        человека победный, то блокируем путем хода ИИ в эту клетку, если победного хода человека нет ходим рандомно.
+         */
+        for (int y = 0; y < helpField.length; y++) {
+            for (int x = 0; x < helpField[y].length; x++) {
+                //Проверяет пуста ли выбранная ячейка, делает предположение и проверяет на выигрыш
+                if(helpField[y][x] == DOT_EMPTY){
+                    helpField[y][x] = DOT_AI;
+                    if (checkWin(DOT_AI, helpField.length, helpField)){
+                        //Если ячейка выигрышная - делает ход и завершает метод
+                        field[y][x] = DOT_AI;
+                        return;
+                    }
+                    //Иначе делает предположение о следующем ходе человека
+                    else {
+                        for (int i = 0; i < field.length; i++) {
+                            for (int j = 0; j < field[i].length; j++) {
+                                if(helpField[i][j] == DOT_EMPTY ){
+                                    helpField[i][j] = DOT_HUMAN;
+                                    //Проверяет выигрывает ли человек на следующем ходу, если выигрывает, то блокирует ход и завершает метод
+                                    if (checkWin(DOT_HUMAN, helpField.length, helpField)){
+                                        field[i][j] = DOT_AI;
+                                        return;
+                                    }
+                                    //Если проигрыша нет - снимает предположение
+                                    helpField[i][j] = DOT_EMPTY;
+                                }
+                            }
+                        }
+                    }
+                    //Если в первой итерации победы и проигрыша нет, снимает предположение
+                    helpField[y][x] = DOT_EMPTY;
+                }
+            }
+        }
+        //Если после 2 ходов нет выигрыша и проигрыша то ходит случайно
         int x;
         int y;
         do {
@@ -74,33 +124,54 @@ public class TicTacToe {
         field[y][x] = DOT_AI;
     }
 
-    /** Проверка победы реализована путем перебора всех игровых ячеек. Считаем сумму совпадений размещенных
-     * на игровом поле отметок от каждой ячейки по горизонтали (вправо), вертикали (вниз) и 2-м диагоналям (вправо вверх
-     * и вправо вниз) с учетом границ игрового поля.
+    /** Проверка выигрыша для любого размера игрового поля и любого необходимого количества отметок в ряд (необходимых
+     * для победы). Проверка реализована путем перебора всех игровых ячеек. Ищет совпадения размещенных
+     * на игровом поле отметок проверяемого игрока от каждой ячейки по горизонтали (вправо), вертикали (вниз) и
+     * 2-м диагоналям (вправо вверх и вправо вниз) с учетом границ игрового поля.
      *
      * @param c отметка проверяемого игрока
-     * @return если победа возращаем истина, иначе - ложь
+     * @param rowSize количество отметок в ряд необходимых для победы
+     * @param checkField Проверяемое игровое поле (необходимо для проверки не только основного поля игры, но и вспомогательного при построении предположений)
+     * @return если победа возращает истина, иначе - ложь
      */
-    private static boolean checkWin(char c) {
-
+    private static boolean checkWin(char c, int rowSize, char[][] checkField) {
+        //Количество отметок в проверяемом ряду (вспомогательная)
+        int sum;
         // Перебор всех ячеек игрового поля
-        for (int y = 0; y < field.length; y++) {
-            for (int x = 0; x < field[y].length; x++) {
+        for (int y = 0; y < checkField.length; y++) {
+            for (int x = 0; x < checkField[y].length; x++) {
+
                 //Проверка по горизонтали вправо (в условии выполняем проверку границы)
-                if (field[y].length - x > 2) {
-                    if (field[y][x] == c && field[y][x+1] == c && field[y][x+2] == c) return true;
+                if (checkField[y].length - x >= rowSize) {
+                    sum = 0;
+                    for (int i = 0; i < rowSize; i++) {
+                        if(checkField[y][x + i] == c) sum++;
+                    }
+                    if (sum == rowSize) return true;
                 }
                 //Проверка по вертикали вниз (в условии выполняем проверку границы)
-                if (field.length - y > 2) {
-                    if (field[y][x] == c && field[y+1][x] == c && field[y+2][x] == c) return true;
+                if (checkField.length - y >= rowSize) {
+                    sum = 0;
+                    for (int i = 0; i < rowSize; i++) {
+                        if(checkField[y + i][x] == c) sum++;
+                    }
+                    if (sum == rowSize) return true;
                 }
                 //Проверка 1 диагонали вправо вверх (в условии выполняем проверку границ)
-                if (y  > 1 && field[y].length - x > 2){
-                    if (field[y][x] == c && field[y-1][x+1] == c && field[y-2][x+2] == c) return true;
+                if (y  >= rowSize - 1 && checkField[y].length - x >= rowSize){
+                    sum = 0;
+                    for (int i = 0; i < rowSize; i++) {
+                        if(checkField[y - i][x + i] == c) sum++;
+                    }
+                    if (sum == rowSize) return true;
                 }
                 //Проверка 2 диагонали вправо вниз (в условии выполняем проверку границ)
-                if (field.length - y > 2 && field[y].length - x > 2) {
-                    if (field[y][x] == c && field[y+1][x+1] == c && field[y+2][x+2] == c) return true;
+                if (checkField.length - y >= rowSize && checkField[y].length - x >= rowSize) {
+                    sum = 0;
+                    for (int i = 0; i < rowSize; i++) {
+                        if(checkField[y + i][x + i] == c) sum++;
+                    }
+                    if (sum == rowSize) return true;
                 }
             }
         }
@@ -131,13 +202,12 @@ public class TicTacToe {
                 if (gameChecks(DOT_AI, "Computer win")) break;
             }
             System.out.println("Play again?");
-            if (!SCANNER.next().equals("Y".toLowerCase()))
-                break;
+            if (!SCANNER.next().toLowerCase().equals("y")) break;
         }
     }
 
     private static boolean gameChecks(char dot, String s) {
-        if (checkWin(dot)) {
+        if (checkWin(dot, field.length, field)) {
             System.out.println(s);
             return true;
         }
